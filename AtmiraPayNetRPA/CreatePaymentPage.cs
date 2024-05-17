8c8837c2a196661437e146b29709520e5cfc391f
palcgut2303 <palcgut2303@g.educaand.es>
2024-05-17 14:54:39 +0200

    Agregar archivos de proyecto.

---
+++ AtmiraPayNetRPA/CreatePaymentPage.cs
@@ -0,0 +1,74 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtmiraPayNetRPA
+{
+    public class CreatePaymentPage
+    {
+        private readonly IWebDriver driver;
+
+        public CreatePaymentPage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        IWebElement btnCreatePayment => driver.FindElement(By.Id("createPayment"));
+        IWebElement OriginAccountIBAN => driver.FindElement(By.Id("OriginAccountIBAN"));
+        IWebElement OriginBankName => driver.FindElement(By.Id("OriginBankName"));
+        IWebElement OriginCountryBank => driver.FindElement(By.Id("OriginCountryBank"));
+        IWebElement CP => driver.FindElement(By.Id("CP"));
+        IWebElement Street => driver.FindElement(By.Id("Street"));
+        IWebElement NumberStreet => driver.FindElement(By.Id("NumberStreet"));
+        IWebElement PayAmount => driver.FindElement(By.Id("PayAmount"));
+        IWebElement DestinationAccountIBAN => driver.FindElement(By.Id("DestinationAccountIBAN"));
+        IWebElement DestinationBankName => driver.FindElement(By.Id("DestinationBankName"));
+        IWebElement DestinationCountryBank => driver.FindElement(By.Id("DestinationCountryBank"));
+        IWebElement InterBankAccountIBAN => driver.FindElement(By.Id("InterBankAccountIBAN"));
+        IWebElement InterBankName => driver.FindElement(By.Id("InterBankName"));
+        IWebElement BtnGenerar => driver.FindElement(By.Id("btnGenerar"));
+
+        IWebElement BtnBorrador => driver.FindElement(By.Id("btnBorrador"));
+
+        public void ClickCreatePayment()
+        {
+            btnCreatePayment.Click();
+        }
+
+        public void CreatePayment(string OriginAccountIBAN, string OriginBankName, string OriginCountryBank, string CP, string Street, string NumberStreet, string PayAmount, string DestinationAccountIBAN, string DestinationBankName, string DestinationCountryBank, string InterBankAccountIBAN, string InterBankName)
+        {
+            this.OriginAccountIBAN.SendKeys(OriginAccountIBAN);
+            this.OriginBankName.SendKeys(OriginBankName);
+            this.OriginCountryBank.SendKeys(OriginCountryBank);
+            this.CP.SendKeys(CP);
+            this.Street.SendKeys(Street);
+            this.NumberStreet.SendKeys(NumberStreet);
+            this.PayAmount.SendKeys(PayAmount);
+            this.DestinationAccountIBAN.SendKeys(DestinationAccountIBAN);
+            this.DestinationBankName.SendKeys(DestinationBankName);
+            this.DestinationCountryBank.SendKeys(DestinationCountryBank);
+
+            if(InterBankAccountIBAN != null && InterBankName != null)
+            {
+                this.InterBankAccountIBAN.SendKeys(InterBankAccountIBAN);
+                this.InterBankName.SendKeys(InterBankName);
+            }
+            
+        }
+
+        public void ClickGenerar()
+        {
+            BtnGenerar.Click();
+        }
+
+        public void ClickBorrador()
+        {
+            BtnBorrador.Click();
+        }
+    }
+}
---
+++ AtmiraPayNetRPA/MainWindow.xaml.cs
@@ -0,0 +1,99 @@
+﻿using OpenQA.Selenium;
+using System.Net;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AtmiraPayNetRPA
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private IWebDriver _webDriver;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            // _webDriver = new OpenQA.Selenium.Chrome.ChromeDriver();
+            //_webDriver.Url = "http://localhost:5058/#";
+        }
+
+        
+
+        private void TestCreatePaymentLetter(object sender, RoutedEventArgs e)
+        {
+            //recoger datos
+            
+
+
+
+        }
+
+        private void TestLoginRegister(object sender, RoutedEventArgs e)
+        {
+            //REGISTER
+            //string username = txtUsername.Text;
+            //string emailRegister = txtEmailRegister.Text;
+            //string passwordRegister = txtPasswordRegister.Text;
+            //string fullName = txtFullname.Text;
+            //DateTime birthdate = calendarBirthdate.SelectedDate.Value;
+
+            //LOGIN
+            string emailLogin = txtEmailLogin.Text;
+            string passwordLogin = txtPasswordLogin.Text;
+
+
+            _webDriver = new OpenQA.Selenium.Chrome.ChromeDriver();
+            _webDriver.Navigate().GoToUrl("http://localhost:5058/");
+            _webDriver.Manage().Window.Maximize();
+
+            Thread.Sleep(6000);
+
+
+
+            //webElement = _webDriver.FindElement(By.Id("username"));
+            //webElement.SendKeys(username);
+
+            //webElement = _webDriver.FindElement(By.Id("fullname"));
+            //webElement.SendKeys(fullName);
+
+            //webElement = _webDriver.FindElement(By.Id("emailRegister"));
+            //webElement.SendKeys(emailRegister);
+
+            //webElement = _webDriver.FindElement(By.Id("passwordRegister"));
+            //webElement.SendKeys(passwordRegister);
+
+            //webElement = _webDriver.FindElement(By.Id("birthdate"));
+            //webElement.SendKeys(birthdate.ToString("dd-MM-yyyy"));
+
+            //webElement = _webDriver.FindElement(By.Id("btnRegister"));
+            //webElement.Click();
+
+            //LOGIN
+            LoginPage loginPage = new LoginPage(_webDriver);
+            loginPage.ClickLogin();
+            loginPage.Login(emailLogin, passwordLogin);
+            
+            Thread.Sleep(6000);
+
+            ListPaymentPage listPaymentPage = new ListPaymentPage(_webDriver);
+            listPaymentPage.ClickHomebtn();
+
+            CreatePaymentPage createPaymentPage = new CreatePaymentPage(_webDriver);
+            createPaymentPage.ClickCreatePayment();
+
+            createPaymentPage.CreatePayment("ES6621000418401234567891", "Santander", "Spain", "28001", "Calle Serrano", "1", "100", "ES6621000418401234567891", "Santander", "Spain", "ES6621000418401234567891", "Santander");
+            createPaymentPage.ClickGenerar();
+
+        }
+    }
+}
