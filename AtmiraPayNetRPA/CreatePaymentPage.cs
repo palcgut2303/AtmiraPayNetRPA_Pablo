b6db2555a5c39897aaa0df6120e3a52c6f4e1691
palcgut2303 <146340194+palcgut2303@users.noreply.github.com>
2024-05-27 14:11:44 +0200

    realizado la creacion de pagos, realizar descargar fichero PDF e indicarle al usuario donde descargarlo

--- AtmiraPayNetRPA/CreatePaymentPage.cs
+++ AtmiraPayNetRPA/CreatePaymentPage.cs
@@ -8,42 +8,47 @@ using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using OpenQA.Selenium.Support.UI;
-
+using OpenQA.Selenium.Interactions;
 
 namespace AtmiraPayNetRPA
 {
     public class CreatePaymentPage
     {
-        private readonly IWebDriver driver;
+        private readonly IWebDriver _driver;
         private WebDriverWait wait;
-
+        private Actions actions;
         public CreatePaymentPage(IWebDriver driver)
         {
-            this.driver = driver;
+           _driver = driver;
 
-            this.driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(15); 
+            _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
 
-
+             wait = new WebDriverWait(_driver, new TimeSpan(0, 0, 5));
+            actions = new Actions(_driver);
         }
 
 
-        IWebElement btnCreatePayment => driver.FindElement(By.Id("createPayment"));
-        IWebElement OriginAccountIBAN => driver.FindElement(By.Id("OriginAccountIBAN"));
-        IWebElement OriginBankName => driver.FindElement(By.Id("OriginBankName"));
 
-        IWebElement OriginCountryBank => driver.FindElement(By.Id("OriginCountryBank"));
-        IWebElement CP => driver.FindElement(By.Id("CP"));
-        IWebElement Street => driver.FindElement(By.Id("Street"));
-        IWebElement NumberStreet => driver.FindElement(By.Id("NumberStreet"));
-        IWebElement PayAmount => driver.FindElement(By.Id("PayAmount"));
-        IWebElement DestinationAccountIBAN => driver.FindElement(By.Id("DestinationAccountIBAN"));
-        IWebElement DestinationBankName => driver.FindElement(By.Id("DestinationBankName"));
-        IWebElement DestinationCountryBank => driver.FindElement(By.Id("DestinationCountryBank"));
-        IWebElement InterBankAccountIBAN => driver.FindElement(By.Id("InterBankAccountIBAN"));
-        IWebElement InterBankName => driver.FindElement(By.Id("InterBankName"));
-        IWebElement BtnGenerar => driver.FindElement(By.Id("btnGenerar"));
+        IWebElement btnCreatePayment => _driver.FindElement(By.Id("createPayment"));
+        IWebElement OriginAccountIBAN => _driver.FindElement(By.Id("OriginAccountIBAN"));
+        IWebElement OriginBankName => _driver.FindElement(By.Id("OriginBankName"));
 
-        IWebElement BtnBorrador => driver.FindElement(By.Id("btnBorrador"));
+        IWebElement OriginCountryBank => _driver.FindElement(By.Id("OriginCountryBank"));
+        IWebElement CP => _driver.FindElement(By.Id("CP"));
+        IWebElement Street => _driver.FindElement(By.Id("Street"));
+        IWebElement NumberStreet => _driver.FindElement(By.Id("NumberStreet"));
+        IWebElement PayAmount => _driver.FindElement(By.Id("PayAmount"));
+        IWebElement DestinationAccountIBAN => _driver.FindElement(By.Id("DestinationAccountIBAN"));
+        IWebElement DestinationBankName => _driver.FindElement(By.Id("DestinationBankName"));
+        IWebElement DestinationCountryBank => _driver.FindElement(By.Id("DestinationCountryBank"));
+        IWebElement InterBankAccountIBAN => _driver.FindElement(By.Id("cuentaIntermediaria"));
+        IWebElement InterBankName => _driver.FindElement(By.Id("nombreBancoIntermediario"));
+        IWebElement BtnGenerar => wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.XPath("//*[@id=\"btnGenerar\"]")));
+
+        IWebElement BtnBorrador => _driver.FindElement(By.Id("btnBorrador"));
+
+        IWebElement BtnYesDialog => wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.XPath("/html/body/div[3]/div/div[6]/button[1]")));
+        IWebElement BtnOkDialog => wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.XPath("/html/body/div[3]/div/div[6]/button[1]")));
 
         public void ClickCreatePayment()
         {
@@ -53,45 +58,55 @@ namespace AtmiraPayNetRPA
 
         public void CreatePayment(string OriginAccountIBAN, string OriginBankName, string OriginCountryBank, string CP, string Street, string NumberStreet, string PayAmount, string DestinationAccountIBAN, string DestinationBankName, string DestinationCountryBank, string InterBankAccountIBAN, string InterBankName)
         {
-            Thread.Sleep(5000);
+                this.OriginAccountIBAN.SendKeys(OriginAccountIBAN);
+                this.OriginBankName.SendKeys(OriginBankName);
 
-            this.OriginAccountIBAN.SendKeys(OriginAccountIBAN);
-            this.OriginBankName.SendKeys(OriginBankName);
+                var selectElement = new SelectElement(this.OriginCountryBank);
+                selectElement.SelectByText(OriginCountryBank);
 
-            var selectElement = new SelectElement(this.OriginCountryBank);
-            selectElement.SelectByText(OriginCountryBank);
+                this.OriginCountryBank.SendKeys(OriginCountryBank);
+                this.CP.SendKeys(CP);
+                this.Street.SendKeys(Street);
+                this.NumberStreet.SendKeys(NumberStreet);
+                this.PayAmount.SendKeys(PayAmount);
+                this.DestinationAccountIBAN.SendKeys(DestinationAccountIBAN);
+                this.DestinationBankName.SendKeys(DestinationBankName);
+                this.DestinationCountryBank.SendKeys(DestinationCountryBank);
 
-            this.OriginCountryBank.SendKeys(OriginCountryBank);
-            this.CP.SendKeys(CP);
-            this.Street.SendKeys(Street);
-            this.NumberStreet.SendKeys(NumberStreet);
-            this.PayAmount.SendKeys(PayAmount);
-            this.DestinationAccountIBAN.SendKeys(DestinationAccountIBAN);
-            this.DestinationBankName.SendKeys(DestinationBankName);
-            this.DestinationCountryBank.SendKeys(DestinationCountryBank);
 
-            
 
-            var selectElement2 = new SelectElement(this.DestinationCountryBank);
-            selectElement2.SelectByText(DestinationCountryBank);
+                var selectElement2 = new SelectElement(this.DestinationCountryBank);
+                selectElement2.SelectByText(DestinationCountryBank);
 
-            if ((InterBankAccountIBAN != "" && InterBankName != ""))
-            {
+                if ((InterBankAccountIBAN != "" && InterBankName != ""))
+                {
 
-                this.InterBankAccountIBAN.SendKeys(InterBankAccountIBAN);
-                this.InterBankName.SendKeys(InterBankName);
-            }
+                    this.InterBankAccountIBAN.SendKeys(InterBankAccountIBAN);
+                    this.InterBankName.SendKeys(InterBankName);
+                }
             
         }
 
         public void ClickGenerar()
         {
-            BtnGenerar.Click();
+            
+                actions.MoveToElement(BtnGenerar).Click().Perform();
+                BtnYesDialog.Click();
+                BtnOkDialog.Click();
+            
         }
 
         public void ClickBorrador()
         {
-            BtnBorrador.Click();
+            
+                actions.MoveToElement(BtnBorrador).Click().Perform();
+                BtnYesDialog.Click();
+                BtnOkDialog.Click();
+            
+
+
         }
+
+
     }
 }
--- AtmiraPayNetRPA/MainWindow.xaml.cs
+++ AtmiraPayNetRPA/MainWindow.xaml.cs
@@ -24,92 +24,129 @@ namespace AtmiraPayNetRPA
         public MainWindow()
         {
             InitializeComponent();
-            // _webDriver = new OpenQA.Selenium.Chrome.ChromeDriver();
-            //_webDriver.Url = "http://localhost:5058/#";
         }
 
-        
 
-        private void TestCreatePaymentLetter(object sender, RoutedEventArgs e)
-        {
-            //recoger datos
-            
-
-
-
-        }
 
         private void TestData(object sender, RoutedEventArgs e)
         {
-
-            //TXT DEL LOGIN
-            string emailLogin = txtEmailLogin.Text;
-            string passwordLogin = txtPasswordLogin.Text;
-
-
-            //TXT DEL PAGO
-            string IBANBankOrigin = txtIBANOrigin.Text;
-            string bankNameOrigin = txtBankNameOrigin.Text;
-            string countryOrigin = txtCountryOrigin.Text;
-
-            string IBANBankDestination = txtIBANDestination.Text;
-            string bankNameDestination = txtBankNameDestination.Text;
-            string countryDestination = txtCountryDestination.Text;
-
-            
-
-            string? IBANBankInter = txtIBANInter.Text;
-            string bankNameInter = txtBankNameInter.Text;
-
-            string CP = txtCp.Text;              
-            string Street = txtStreet.Text;
-            string NumberStreet = txtNumberStreet.Text;
-            string PayAmount = txtPayAmount.Text;
-
-
-
-            _webDriver = new OpenQA.Selenium.Chrome.ChromeDriver();
-            _webDriver.Navigate().GoToUrl("http://localhost:5058/");
-
-            _webDriver.Manage().Timeouts().ImplicitWait = System.TimeSpan.FromSeconds(8);
-
-            _webDriver.Manage().Window.Maximize();
-
-            //LOGIN
-            LoginPage loginPage = new LoginPage(_webDriver);
-            loginPage.ClickLogin();
-            loginPage.Login(emailLogin, passwordLogin);
-            
-            
-
-            ListPaymentPage listPaymentPage = new ListPaymentPage(_webDriver);
-            listPaymentPage.ClickHomebtn();
-
-            CreatePaymentPage createPaymentPage = new CreatePaymentPage(_webDriver);
-            createPaymentPage.ClickCreatePayment();
-
-            
-
-
-            createPaymentPage.CreatePayment(IBANBankOrigin, bankNameOrigin, countryOrigin, CP, Street, NumberStreet, PayAmount, IBANBankDestination, bankNameDestination, countryDestination, IBANBankInter, bankNameInter);
-
-            ComboBoxItem selectedItem = comboBoxTypeOperation.SelectedItem as ComboBoxItem;
-
-            string tipoOperacion =  selectedItem!.Content.ToString()!;
-
-            
-
-            if (tipoOperacion == "GENERAR")
+            try
             {
-               createPaymentPage.ClickGenerar();
+                //TXT DEL LOGIN
+                string emailLogin = txtEmailLogin.Text;
+                string passwordLogin = txtPasswordLogin.Text;
+
+
+                //TXT DEL PAGO
+                string IBANBankOrigin = txtIBANOrigin.Text;
+                string bankNameOrigin = txtBankNameOrigin.Text;
+                string countryOrigin = txtCountryOrigin.Text;
+
+                string IBANBankDestination = txtIBANDestination.Text;
+                string bankNameDestination = txtBankNameDestination.Text;
+                string countryDestination = txtCountryDestination.Text;
+
+
+
+                string? IBANBankInter = txtIBANInter.Text;
+                string bankNameInter = txtBankNameInter.Text;
+
+                string CP = txtCp.Text;
+                string Street = txtStreet.Text;
+                string NumberStreet = txtNumberStreet.Text;
+                string PayAmount = txtPayAmount.Text;
+
+
+                _webDriver = new OpenQA.Selenium.Chrome.ChromeDriver();
+                _webDriver.Navigate().GoToUrl("http://localhost:5058/");
+
+                _webDriver.Manage().Timeouts().ImplicitWait = System.TimeSpan.FromSeconds(5);
+
+                _webDriver.Manage().Window.Maximize();
+
+                //LOGIN
+                LoginPage loginPage = new LoginPage(_webDriver);
+                loginPage.ClickLogin();
+                loginPage.Login(emailLogin, passwordLogin);
+
+
+
+                ListPaymentPage listPaymentPage = new ListPaymentPage(_webDriver);
+                listPaymentPage.ClickHomebtn();
+
+                CreatePaymentPage createPaymentPage = new CreatePaymentPage(_webDriver);
+                createPaymentPage.ClickCreatePayment();
+
+
+
+
+                createPaymentPage.CreatePayment(IBANBankOrigin, bankNameOrigin, countryOrigin, CP, Street, NumberStreet, PayAmount, IBANBankDestination, bankNameDestination, countryDestination, IBANBankInter, bankNameInter);
+
+                ComboBoxItem selectedItem = comboBoxTypeOperation.SelectedItem as ComboBoxItem;
+
+                string tipoOperacion = selectedItem!.Content.ToString()!;
+
+                if (tipoOperacion == "GENERAR")
+                {
+                    createPaymentPage.ClickGenerar();
+                }
+                else
+                {
+                    createPaymentPage.ClickBorrador();
+                }
             }
-            else
+            catch (NoSuchElementException ex)
             {
-                createPaymentPage.ClickBorrador();
+                // Manejar la excepción si no se encuentra el elemento
+                ShowError("Error: El elemento no se encontró. ", ex);
             }
+            catch (ElementClickInterceptedException ex)
+            {
+                // Manejar la excepción si el elemento no es clickeable
+                ShowError("Error: El elemento no es clickeable. " ,ex);
+            }
+            catch (Exception ex)
+            {
+                ShowError("Error: Ocurrió un error inesperado. ", ex);
+            }
+
+
 
 
         }
 
+        private void ShowError(string message, Exception ex)
+        {
+            // Mostrar el mensaje de error en un MessageBox
+            MessageBox.Show(message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+
+
+            // Mostrar el mensaje de error en el TextBlock
+            ErrorTextBlock.Text = $"{message}\n{ex.Message}";
+
+            ErrorLabel.Visibility = Visibility.Visible;
+            ErrorTextBlock.Visibility = Visibility.Visible;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFolderDialog dialog = new();
+
+            dialog.Multiselect = false;
+            dialog.Title = "Select a folder";
+
+            // Show open folder dialog box
+            bool? result = dialog.ShowDialog();
+
+            // Process open folder dialog box results
+            if (result == true)
+            {
+                // Get the selected folder
+                string fullPathToFolder = dialog.FolderName;
+                string folderNameOnly = dialog.SafeFolderName;
+
+                txtPathFolder.Text = fullPathToFolder;
+            }
+        }
     }
 }
